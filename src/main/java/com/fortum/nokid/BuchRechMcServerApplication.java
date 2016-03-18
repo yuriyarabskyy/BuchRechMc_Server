4705351a1c1fb81c1746f046b5912bbb296e32fe
kolnol <odnoshivkin@me.com>
2016-03-18 16:31:11 +0100

    Initial commit

---
+++ src/main/java/com/fortum/nokid/BuchRechMcServerApplication.java
@@ -0,0 +1,12 @@
+package com.fortum.nokid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchRechMcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchRechMcServerApplication.class, args);
+	}
+}
