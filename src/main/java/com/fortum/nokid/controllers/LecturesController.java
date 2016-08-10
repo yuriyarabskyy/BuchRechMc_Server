ebb9a8e1f90d088cc5c7f72c528b70a6285fe9a4
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2016-08-11 00:27:33 +0300

    HUUUUUGE renew database

---
+++ src/main/java/com/fortum/nokid/controllers/LecturesController.java
@@ -0,0 +1,7 @@
+package com.fortum.nokid.controllers;
+
+/**
+ * Created by yuriy on 10/08/16.
+ */
+public class LecturesController {
+}
---
+++ src/main/java/com/fortum/nokid/entities/Lecture.java
@@ -0,0 +1,7 @@
+package com.fortum.nokid.entities;
+
+/**
+ * Created by yuriy on 10/08/16.
+ */
+public class Lecture {
+}
